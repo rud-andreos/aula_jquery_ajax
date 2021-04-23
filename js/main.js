d78e75d00fc27f0c63982ca738b27fc5c25a53f9
rud-andreos <rud.andreos@gmail.com>
2021-04-23 18:13:03 -0300

    2021_04_23_consulta_cep_v1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function consultaCep(){
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/"
     console.log(url);
@@ -12,6 +13,13 @@ function consultaCep(){
             $("#localidade").html(response.localidade);
             $("#uf").html(response.uf);
             $("#titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
